649204422eaefd952997f8816ad3b2793b3c849e
Volodymyr Masliy <thebestvova@gmail.com>
2024-12-02 07:32:19 +0100

    extract isUnsafe

--- day2/main.go
+++ day2/main.go
@@ -6,34 +6,34 @@ import (
 	"vmas/advent2024/utils"
 )
 
+func isUnsafe(prev, curr int, increasing bool) bool {
+	if prev == curr || utils.Difference(prev, curr) > 3 {
+		return true
+	}
+
+	if (increasing && curr < prev) || (!increasing && curr > prev) {
+		return true
+	}
+
+	return false
+}
+
 func main() {
 	safe := 0
 	for _, line := range utils.ReadLines(os.Args[1]) {
 		report := utils.IntFields(line)
 
 		reportSafe := true
-		inc := report[0] < report[(len(report) - 1)]
+		inc := report[0] < report[(len(report)-1)]
 		for i := 1; i < len(report); i++ {
-			prev := report[i - 1]
+			prev := report[i-1]
 			curr := report[i]
 
-			if (utils.Difference(prev, curr) > 3)  {
+			if isUnsafe(prev, curr, inc) {
 				reportSafe = false
 				break
 			}
 
-			if (prev == curr) {
-				reportSafe = false
-				break
-			}
-
-			if (inc && curr < prev) {
-				reportSafe = false
-				break
-			} else if (!inc && curr > prev) {
-				reportSafe = false
-				break
-			}
 		}
 
 		if reportSafe {
