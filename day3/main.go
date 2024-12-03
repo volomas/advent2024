49fef45b75f7846444d2e784b3115ab1546b9fed
Volodymyr Masliy <thebestvova@gmail.com>
2024-12-03 07:32:20 +0100

    day3 part2

--- day3/main.go
+++ day3/main.go
@@ -15,20 +15,46 @@ func main() {
 		panic(err)
 	}
 	content := string(bytes)
-
-	mul, err := regexp.Compile(`mul\([0-9]+,[0-9]+\)`)
-	if err != nil {
-		panic(err)
-	}
-	muls := mul.FindAllString(content, -1)
+	do := true
 	sum := 0
-	for _, m := range muls {
-		args := strings.Split(m, ",")
-		a1, _ := strings.CutPrefix(args[0], "mul(")
-		a2, _ := strings.CutSuffix(args[1], ")")
-		n1 := utils.Must(strconv.Atoi(a1))
-		n2 := utils.Must(strconv.Atoi(a2))
-		sum += n1 * n2
+
+	for i, char := range content {
+		if char == 'd' {
+			enable, ok := parseEnableAt(content, i)
+			if (ok) {
+				do = enable
+			}
+		}
+
+		if do && char == 'm' {
+			x, y := parseMulAt(content, i)
+			sum += x * y
+		}
 	}
 	fmt.Println(sum)
+}
+
+// parse don't() or do()
+func parseEnableAt(content string, idx int) (bool, bool) {
+	maybeDontOrDo := string(content[idx:idx+7])
+	if (strings.Contains(maybeDontOrDo, "don't()")) {
+		return false, true
+	} else if (strings.Contains(maybeDontOrDo, "do()")) {
+		return true, true
+	}
+	return false, false
+}
+
+// parse mul(x,y)
+func parseMulAt(content string, idx int) (int, int) {
+	mul := utils.Must(regexp.Compile(`^mul\([0-9]+,[0-9]+\)`))
+	found := mul.FindString(content[idx:])
+	if found == "" {
+		return 0, 0
+	} else {
+		found, _ = strings.CutPrefix(found, "mul(")
+		found, _ = strings.CutSuffix(found, ")")
+		args := strings.Split(found, ",")
+		return utils.Must(strconv.Atoi(args[0])), utils.Must(strconv.Atoi(args[1]))
+	}
 }
