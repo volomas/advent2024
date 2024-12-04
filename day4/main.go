f028990309e8820a2e8bace626b73f412ec0930b
Volodymyr Masliy <thebestvova@gmail.com>
2024-12-04 12:02:13 +0100

    day4 part2

--- day4/main.go
+++ day4/main.go
@@ -8,55 +8,13 @@ import (
 
 var lines []string
 
-type Direction int
-
-const (
-	N Direction = iota
-	S
-	W
-	E
-	NW
-	NE
-	SW
-	SE
-)
-
-var allDirections = []Direction{N, S, W, E, NW, NE, SW, SE}
-
-func (d Direction) next(r, c int) (int, int) {
-	switch d {
-	case N:
-		return r - 1, c
-	case S:
-		return r + 1, c
-	case W:
-		return r, c - 1
-	case E:
-		return r, c + 1
-	case NW:
-		return r - 1, c - 1
-	case NE:
-		return r - 1, c + 1
-	case SW:
-		return r + 1, c - 1
-	case SE:
-		return r + 1, c + 1
-	default:
-		return -1, -1
-	}
-}
-
 func main() {
 	lines = utils.ReadLines(os.Args[1])
 	xmasCount := 0
-	for r, line := range lines {
-		for c, letter := range line {
-			if letter == 'X' {
-				for _, dir := range allDirections {
-					if findXmas(' ', r, c, dir) {
-						xmasCount += 1
-					}
-				}
+	for r := 2; r < len(lines); r++ {
+		for c := 2; c < len(lines[0]); c++ {
+			if isXmas(r, c) {
+				xmasCount += 1
 			}
 		}
 	}
@@ -64,28 +22,20 @@ func main() {
 	fmt.Println(xmasCount)
 }
 
-// dir
-func findXmas(prevLetter rune, r, c int, dir Direction) bool {
-	if r < 0 || c < 0 || r >= len(lines) || c >= len(lines[0]) {
-		return false
-	}
-	currentLetter := rune(lines[r][c])
-	if currentLetter == 'S' && prevLetter == 'A' {
-		return true
-	}
-
-	if r < 0 || c < 0 || r >= len(lines) || c >= len(lines[0]) {
+func isXmas(r, c int) bool {
+	center := rune(lines[r-1][c-1])
+	if center != 'A' {
 		return false
 	}
 
-	nextR, nextC := dir.next(r, c)
-	if currentLetter == 'X' && prevLetter == ' ' {
-		return findXmas(currentLetter, nextR, nextC, dir)
-	} else if currentLetter == 'M' && prevLetter == 'X' {
-		return findXmas(currentLetter, nextR, nextC, dir)
-	} else if currentLetter == 'A' && prevLetter == 'M' {
-		return findXmas(currentLetter, nextR, nextC, dir)
-	} else {
-		return false
-	}
+	a1 := rune(lines[r-2][c-2])
+	a2 := rune(lines[r][c])
+
+	b1 := rune(lines[r-2][c])
+	b2 := rune(lines[r][c-2])
+
+	d1 := (a1 == 'M' && a2 == 'S') || (a1 == 'S' && a2 == 'M')
+	d2 := (b1 == 'M' && b2 == 'S') || (b1 == 'S' && b2 == 'M')
+
+	return d1 && d2
 }
